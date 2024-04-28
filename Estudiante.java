4787d62d319dd708572eb63398f11258f6c56561
Leonardo Mejía <leomejia646@gmail.com>
2024-04-28 10:09:32 -0600

    Creación de clases para gestionar usuarios

--- Estudiante.java
+++ Estudiante.java
@@ -1,16 +1,44 @@
 import java.util.List;
 
 public class Estudiante {
-    String nombre;
-    List<String> intereses;
-    List<String> clubesAsistidos;
+    private String nombre;
+    private List<String> intereses;
+    private List<String> clubesAsistidos;
     //Agregar pérfil psicológico
     //Agregar disponibilidad de horario
-    //Agregar 
+    //Agregar carrera 
+    //Agregar edad
+    //Agregar género
+    
+
 
     public Estudiante(String nombre, List<String> intereses, List<String> clubesAsistidos) {
         this.nombre = nombre;
         this.intereses = intereses;
+        this.clubesAsistidos = clubesAsistidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<String> getIntereses() {
+        return intereses;
+    }
+
+    public void setIntereses(List<String> intereses) {
+        this.intereses = intereses;
+    }
+
+    public List<String> getClubesAsistidos() {
+        return clubesAsistidos;
+    }
+
+    public void setClubesAsistidos(List<String> clubesAsistidos) {
         this.clubesAsistidos = clubesAsistidos;
     }
 }
--- Main.java
+++ Main.java
@@ -4,9 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    
-
     public static void main(String[] args) {
+        GestorUsuarios gestorUsuarios = new GestorUsuarios();
         // Crear instancias de estudiantes y clubes (datos de ejemplo)
         Estudiante juan = new Estudiante("Juan", Arrays.asList("Musical", "Deportiva"), Arrays.asList("Marimba", "Futsal"));
         Estudiante maria = new Estudiante("Maria", Arrays.asList("Musical", "Artística"), Arrays.asList("Coro Voces del Valle"));
@@ -16,6 +15,18 @@ public class Main {
         grafo.put("Futsal", new Club("Futsal", "Deportiva", Arrays.asList("Lunes y miércoles 15:00-16:30")));
         grafo.put("Coro Voces del Valle", new Club("Coro Voces del Valle", "Musical", Arrays.asList("Martes 15:30-17:30", "Sábado 13:00-15:00")));
     
+        gestorUsuarios.registrarUsuario("Majo", "12345", maria);
+        
+        gestorUsuarios.registrarUsuario("Juan", "67890", juan);
+
+        Usuario usuario = gestorUsuarios.iniciarSesion("Majo", "12345");
+        if (usuario != null) {
+            System.out.println("Bienvenido, " + usuario.getNombreUsuario() + "!");
+            System.out.println("Nombre del estudiante asociado: " + usuario.getEstudiante().getNombre());
+        } else {
+            System.out.println("Inicio de sesión fallido.");
+        }
+
         // Recomendar clubes para los estudiantes
         RecomendadorClubes recomendador = new RecomendadorClubes();
         List<String> recomendacionesJuan = recomendador.recomendacionClubes(juan, grafo);
